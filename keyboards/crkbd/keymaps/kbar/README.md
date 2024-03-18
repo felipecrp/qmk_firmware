4a12d0eebcf9712dae579e32fd859f9def576b57
Felipe Curty <felipecrp@gmail.com>
2024-03-18 16:54:30 -0300

    Add keyoveride to S-BSPC and ajust layers

---
+++ keyboards/crkbd/keymaps/kbar/README.md
@@ -0,0 +1,4 @@
+# Compile Instructions
+
+qmk compile -kb crkbd -km default -e CONVERT_TO=promicro_rp2040
+
--- keyboards/crkbd/keymaps/kbar/keymap.c
+++ keyboards/crkbd/keymaps/kbar/keymap.c
@@ -77,6 +77,12 @@ combo_t key_combos[] = {
   [IOP_GAM] = COMBO(iop_combo, TG(KBAR_GAM))
 };
 
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, MT_BSPC, KC_DEL);
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &delete_key_override,
+    NULL // Null terminate the array of overrides!
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [KBAR_BSE] = LAYOUT_split_3x6_3( 
@@ -101,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [KBAR_MSE] = LAYOUT_split_3x6_3(
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_ACL2,  _______,
+    _______,  _______,  _______,  _______,  _______,  QK_BOOT,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_ACL2,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  KC_MS_L,  KC_MS_D,  KC_MS_U,  KC_MS_R,  KC_ACL1,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  KC_WH_L,  KC_WH_D,  KC_WH_U,  KC_WH_R,  KC_ACL0,  _______,
                                   _______,  _______,  _______,  KC_BTN1,  KC_BTN2,  KC_BTN3
@@ -124,15 +130,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [KBAR_SYM] = LAYOUT_split_3x6_3(
     _______,  _______,  KC_CIRC,  _______,  KC_GRV,   _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  KC_TILD,  KC_DQUO,  KC_QUOT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  KC_GRV,   KC_CEDIL, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  KC_CEDIL, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
                                   _______,  _______,  _______,  _______,  _______,  _______   
 ),
 
 [KBAR_SY2] = LAYOUT_split_3x6_3(
     _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  KC_AMPR,  KC_RABK,  KC_LABK,  KC_LPRN,  KC_RPRN,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  KC_BSLS,  KC_RBRC,  KC_LBRC,  KC_LCBR,  KC_RCBR,  _______,  _______,  _______,  _______,  _______,  _______,
-                                  KC_COLN,  KC_MINS,  KC_EQL,  _______,  _______,  _______   
+    _______,  KC_AMPR,  KC_LABK,  KC_RABK,  KC_LPRN,  KC_RPRN,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  KC_BSLS,  KC_LBRC,  KC_RBRC,  KC_LCBR,  KC_RCBR,  _______,  _______,  _______,  _______,  _______,  _______,
+                                  KC_COLN,  KC_MINS,  KC_EQL,   _______,  _______,  _______   
 ),
 
 [KBAR_FUN] = LAYOUT_split_3x6_3(
